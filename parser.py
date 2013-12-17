bcda6bddb1b449d682e5c7c32fa70c3392299674
Zach Wick <zach@zachwick.com>
2013-12-17 09:18:04 -0500

    Added license info and some README content
    Signed-off-by: Zach Wick <zach@zachwick.com>

--- parser.py
+++ parser.py
@@ -1,3 +1,25 @@
+'''
+plink - a super simple web browser
+
+Copyright 2013 ZachWick <zach@zachwick.com>
+
+This file is part of plink.
+
+plink is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+plink is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with plink.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
+
 from html.parser import HTMLParser
 from html.entities import name2codepoint
 import urllib3
